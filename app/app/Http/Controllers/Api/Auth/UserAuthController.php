537708194fe17e3b1176148fd512e53e6e042c50
Iman Borumand Zadeh <imanborumand@gmail.com>
2022-12-27 12:51:54 +0000

    add code arg to OtpInterface

--- app/app/Http/Controllers/Api/Auth/UserAuthController.php
+++ app/app/Http/Controllers/Api/Auth/UserAuthController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Classes\Otp\OtpFactory;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\User\StepOneLoginRequest;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class UserAuthController extends Controller
 {
@@ -17,11 +21,36 @@ class UserAuthController extends Controller
     public function stepOne( StepOneLoginRequest $request) : mixed
     {
         //todo save or update user
-        //todo generate login code
 
-        (new OtpFactory())->generate($request->type)->send($request->value);
+
+        //generate random code
+        $code = Str::random(6);
+
+        //save code to cache for 2 min
+        Cache::put($request->value,
+                   ['type' => $request->type, 'code' => $code],
+                   Carbon::now()->addMinutes(2));
+
+
+        (new OtpFactory())->generate($request->type)->send($request->value, $code);
 
         return 'success';
+    }
+
+
+
+    public function login( Request $request)
+    {
+        //todo get code from request
+        $code = Cache::get($request->value);
+        if (!$code) {
+            return 'code not valid';
+        }
+
+        //todo get user buy
+
+
+
     }
 
 }
